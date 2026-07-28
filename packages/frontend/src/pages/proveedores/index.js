fe09db698fd2531f2d2f7598ddac600f6f5a235b
Jorge Rojas <jorgeluisrojasb@gmail.com>
2026-07-28 19:07:26 -0400

    refactor: rename Providers sidebar item to Desglose

--- packages/frontend/src/pages/proveedores/index.js
+++ packages/frontend/src/pages/proveedores/index.js
@@ -96,7 +96,7 @@ const ProveedoresPage = () => {
                                     <polyline points="3.27 6.96 12 12.01 20.73 6.96" />
                                     <line x1="12" y1="22.08" x2="12" y2="12" />
                                 </svg>
-                                Proveedores
+                                Desglose
                             </Nav.Link>
                         </Nav.Item>
                         {/* <Nav.Item> */}
